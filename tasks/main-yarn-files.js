513cbc36f30b6f1d30c8a6a8008b555c41e9f552
aimad.majdou <aimad.majdou@gmail.com>
2017-11-08 10:47:03 +0000

    0.0.2

--- tasks/main-yarn-files.js
+++ tasks/main-yarn-files.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     var mainBowerFiles = require('..');
     var vfs = require('vinyl-fs');
 
-    grunt.registerMultiTask('bower', 'Copy Bower packages to the destination folder.', function() {
+    grunt.registerMultiTask('yarn', 'Copy Yarn packages to the destination folder.', function() {
         var done = this.async();
         var options = this.options() || {};
         var targets = (this.file) ? [this.file] : this.files;
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
             var base = target.base;
             var dest = target.dest;
 
-            vfs.src(mainBowerFiles(options), {base: base})
+            vfs.src(mainYarnFiles(options), {base: base})
                 .pipe(vfs.dest(dest))
                 .on('end', function() {
                     done();
